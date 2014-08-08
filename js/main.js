fae5597aebd02b38be097c962a891aa31b849d60
PJ Targun <pjtargun@gmail.com>
2014-08-08 13:38:57 -0500

    New Video

--- js/main.js
+++ js/main.js
@@ -92,22 +92,7 @@ $('.chart').waypoint(function() {
 
 /* VEGAS Home Slider */
 	
-		$.vegas('slideshow', {
-			  backgrounds:[
-				
-				{ src:'img/slider/02.jpg', fade:2000 },
-				{ src:'img/slider/01.png', fade:1000 },
-				{ src:'img/slider/04.png', fade:1000 }
-			  ]
-			})('overlay', {
-			  src:'img/overlays/16.png'
-			});
-			$( "#vegas-next" ).click(function() {
-			  $.vegas('next');
-			});
-			$( "#vegas-prev" ).click(function() {
-			  $.vegas('previous');
-		});
+
 
 
 /*Contact form */
